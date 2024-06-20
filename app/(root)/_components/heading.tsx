88953749045286281704d98347b863d0d9e7313b
Favour Idi <idifavour7@gmail.com>
2024-06-20 11:12:29 +0100

    edit

--- app/(root)/_components/heading.tsx
+++ app/(root)/_components/heading.tsx
@@ -5,7 +5,7 @@ import { ArrowRight } from "lucide-react";
 
 export const Heading = () => {
   return (
-    <div className="max-w-3xl space-y-8">
+    <div className="max-w-3xl space-y-10 md:space-y-8">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold primary-font">
         {/* All-in-one <span className="italic">event platform </span>   for you{" "}
         <span className="underline">Outinz</span> */}
